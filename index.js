a75302ef82226775735cd974dcb5a87ce6dd4c68
Tom Tarpey <tomtarpeydev@gmail.comn>
2022-08-12 17:24:26 +0100

    index get request for cat list

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Cats frontend");
+const catList = document.querySelector(".catlist");
+function populatecatList(catData) {
+
+    for (let cat of catData) {
+      let li = document.createElement("li");
+      let a = document.createElement("a");
+      let text = document.createTextNode(cat.name);
+  
+      a.setAttribute('href', `./cat.html?id=${cat.id}`);
+  
+      a.appendChild(text);
+      li.appendChild(a);
+      catList.appendChild(li);
+    }
+}
+window.onload = async function(evt) {
+    evt.preventDefault();
+    console.log("Getting Cat Data...");
+    axios.get("http://workshop-dev.us-west-2.elasticbeanstalk.com/cats")
+    .then((res) => {
+      console.log(res.data);
+      populatecatList(res.data);
+    })
+}
